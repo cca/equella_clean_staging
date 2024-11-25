c9e4223e496c9d952ff38066800a6a99b06ff2f9
phette23 <phette23@gmail.com>
2024-11-25 11:14:58 -0800

    fix: sync config.py not json

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # sync relevant files to EQUELLA app nodes
-rsync -avz config.json clean.py data v2:~/clean-staging
-rsync -avz config.json clean.py data v1:~/clean-staging
+rsync -avz config.py clean.py data v2:~/clean-staging
+rsync -avz config.py clean.py data v1:~/clean-staging
